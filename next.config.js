74ee9af7583bb7d1e778d19ae93c5024f829dc64
Henry Munoz <alexcol123@yahoo.com>
2019-10-28 23:24:37 -0400

    11 Configure Mongo atlas   and connect to database

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
-    JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    MONGO_SRV:
+      'mongodb+srv://Alexcol123:Alex1960@reactreserve-9mdxh.mongodb.net/test?retryWrites=true&w=majority',
+    JWT_SECRET: '<insert-jwt-secret>',
+    CLOUDINARY_URL: '<insert-cloudinary-url>',
+    STRIPE_SECRET_KEY: '<insert-stripe-secret-key>'
   }
 };
