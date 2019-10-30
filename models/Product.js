cad3c4599accda14c218f81b0f0722d11d1e1361
Henry Munoz <alexcol123@yahoo.com>
2019-10-29 21:26:23 -0400

    Create Product collection , model product data

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from 'mongoose';
+import shortId from 'shortid';
+
+const { String, Number } = mongoose.Schema.Types;
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  sku: {
+    type: String,
+    unique: true,
+    default: shortId.generate()
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  mediaUrl: {
+    type: String,
+    required: true
+  }
+});
+
+export default mongoose.models.Product ||
+  mongoose.model('Product', ProductSchema);
