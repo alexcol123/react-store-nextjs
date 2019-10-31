0e5f35c23acd7333be03bf52357d20ccb80e4149
Henry Munoz <alexcol123@yahoo.com>
2019-10-31 10:16:37 -0400

    Base Url Helper

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import baseUrl from '../utils/baseUrl';
 
 function Home({ products }) {
- 
-
   return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async () => {
   // Fetch data on server
-  const url = 'http://localhost:3000/api/products';
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
 
