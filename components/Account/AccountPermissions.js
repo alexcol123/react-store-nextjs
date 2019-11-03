5d8ba93ca6f1f41d529282cfa16ab22fae6d74ed
Henry Munoz <alexcol123@yahoo.com>
2019-11-03 16:20:44 -0500

    39. Change User Roles, Permissions

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -1,8 +1,8 @@
-import React from "react";
-import axios from "axios";
-import { Header, Checkbox, Table, Icon } from "semantic-ui-react";
-import cookie from "js-cookie";
-import baseUrl from "../../utils/baseUrl";
+import React from 'react';
+import axios from 'axios';
+import { Header, Checkbox, Table, Icon } from 'semantic-ui-react';
+import cookie from 'js-cookie';
+import baseUrl from '../../utils/baseUrl';
 
 function AccountPermissions() {
   const [users, setUsers] = React.useState([]);
@@ -13,14 +13,14 @@ function AccountPermissions() {
 
   async function getUsers() {
     const url = `${baseUrl}/api/users`;
-    const token = cookie.get("token");
+    const token = cookie.get('token');
     const payload = { headers: { Authorization: token } };
     const response = await axios.get(url, payload);
     setUsers(response.data);
   }
 
   return (
-    <div style={{ margin: "2em 0" }}>
+    <div style={{ margin: '2em 0' }}>
       <Header as="h2">
         <Icon name="settings" />
         User Permissions
@@ -48,16 +48,38 @@ function AccountPermissions() {
 }
 
 function UserPermission({ user }) {
+  const [admin, setAdmin] = React.useState(user.role === 'admin');
+
+  const isFirstRun = React.useRef(true);
+
+  React.useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+    updatePermission();
+  }, [admin]);
+
+  function handleChangePermission() {
+    setAdmin(prevState => !prevState);
+  }
+
+  async function updatePermission() {
+    const url = `${baseUrl}/api/account`;
+    const payload = { _id: user._id, role: admin ? 'admin' : 'user' };
+    await axios.put(url, payload);
+  }
+
   return (
     <Table.Row>
       <Table.Cell collapsing>
-        <Checkbox toggle />
+        <Checkbox checked={admin} toggle onChange={handleChangePermission} />
       </Table.Cell>
       <Table.Cell>{user.name}</Table.Cell>
       <Table.Cell>{user.email}</Table.Cell>
       <Table.Cell>{user.createdAt}</Table.Cell>
       <Table.Cell>{user.updatedAt}</Table.Cell>
-      <Table.Cell>{user.role}</Table.Cell>
+      <Table.Cell>{admin ? 'admin' : 'user'}</Table.Cell>
     </Table.Row>
   );
 }
--- pages/api/account.js
+++ pages/api/account.js
@@ -1,12 +1,26 @@
-import User from "../../models/User";
-import jwt from "jsonwebtoken";
-import connectDb from "../../utils/connectDb";
+import User from '../../models/User';
+import jwt from 'jsonwebtoken';
+import connectDb from '../../utils/connectDb';
 
 connectDb();
 
 export default async (req, res) => {
-  if (!("authorization" in req.headers)) {
-    return res.status(401).send("No authorization token");
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res);
+      break;
+    case 'PUT':
+      await handlePutRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+async function handleGetRequest(req, res) {
+  if (!('authorization' in req.headers)) {
+    return res.status(401).send('No authorization token');
   }
 
   try {
@@ -18,9 +32,15 @@ export default async (req, res) => {
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).send("User not found");
+      res.status(404).send('User not found');
     }
   } catch (error) {
-    res.status(403).send("Invalid token");
+    res.status(403).send('Invalid token');
   }
-};
+}
+
+async function handlePutRequest(req, res) {
+  const { _id, role } = req.body;
+  await User.findOneAndUpdate({ _id }, { role });
+  res.status(203).send('User updated');
+}
